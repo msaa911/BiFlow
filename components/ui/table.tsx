1db6b542d32dd6fbc2334e9d9822714df85e536a
msaa911 <miguelhsaa@gmail.com>
2026-02-23 19:33:33 -0300

    Fix: Error sintáctico en componente Table UI

--- components/ui/table.tsx
+++ components/ui/table.tsx
@@ -10,7 +10,7 @@ const Table = React.forwardRef<
             ref={ref}
             className={cn("w-full caption-bottom text-sm", className)}
             {...props}
-    )
+        />
     </div>
 ))
 Table.displayName = "Table"
