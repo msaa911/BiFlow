c638a78e30b16d85b30774aaee3736a01824b170
msaa911 <miguelhsaa@gmail.com>
2026-02-26 00:28:10 -0300

    fix: add missing DialogDescription import in invoice-import-preview-modal

--- components/dashboard/invoice-import-preview-modal.tsx
+++ components/dashboard/invoice-import-preview-modal.tsx
@@ -6,6 +6,7 @@ import {
     DialogContent,
     DialogHeader,
     DialogTitle,
+    DialogDescription,
     DialogFooter,
 } from '@/components/ui/dialog'
 import { Button } from '@/components/ui/button'
